53825106c347dd8bbc6f6ca2e19e6188bff8c75c
pavan Kumar Naraharasetty <racedash9@gmail.com>
2024-08-04 17:01:01 +0530

    intial development of current scale directive

---
+++ projects/mapuiexts/ng-ol-material/src/lib/directives/text/current-scale.directive.spec.ts
@@ -0,0 +1,8 @@
+import { CurrentScaleDirective } from './current-scale.directive';
+
+describe('CurrentScaleDirective', () => {
+  it('should create an instance', () => {
+    const directive = new CurrentScaleDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ projects/mapuiexts/ng-ol-material/src/lib/directives/text/current-scale.directive.ts
@@ -0,0 +1,11 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[nolmCurrentScale]',
+  standalone: true
+})
+export class CurrentScaleDirective {
+
+  constructor() { }
+
+}
